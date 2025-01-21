b3bf797b14105aab2212757b3d6bfd94c2a9725e
Erick Jovan <erickjovan106@gmail.com>
2025-01-21 00:24:14 -0600

    add: implement sleep helper and integrate with LabelPicker for async data fetching

--- tanstack-query-issues/src/issues/components/LabelPicker.tsx
+++ tanstack-query-issues/src/issues/components/LabelPicker.tsx
@@ -1,4 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
+import { sleep } from '../../helpers';
+
+const getLabels = async (): Promise<unknown[]> => {
+  await sleep(1000)
+  
+  const response = await fetch('https://api.github.com/repos/facebook/react/labels')
+    .then((r) => r.json())
+
+  console.log((response))
+
+  return response.json()
+}
+
 export const LabelPicker = () => {
+  const labelsQuery = useQuery({
+    queryKey: ['labels'],
+    queryFn: getLabels, // Cualquier promesa, no necesariamente una peticón http
+  })
+
+  if (labelsQuery.isLoading) {
+    return <div className='flex justify-center items-center h-52'>Espere...</div>
+  }
+  
   return (
     <>
       <span
